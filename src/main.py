2bf2036750eb9caa33f30f9630c07b6fa4aa5d42
Attachai-S <attachai.s.work@gmail.com>
2025-03-05 12:19:03 +0700

    add read image

--- src/main.py
+++ src/main.py
@@ -1,4 +1,18 @@
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
+
+img = cv2.imread("images/acne2.jpg")
+grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(10,10))
+plt.subplot(1,2,1)
+plt.imshow(img);plt.axis('off');plt.title('Original Image')
+
+plt.subplot(1,2,2)
+plt.imshow(grey, cmap='gray')
+plt.axis('off');plt.title('Grayscale Image')
+
+plt.show()
