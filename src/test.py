9ab4145056e1b9706f3c278227f9691cf3e7c84f
Attachai-S <attachai.s.work@gmail.com>
2025-03-05 13:57:03 +0700

    add extract facial on main.py and test.py

--- src/test.py
+++ src/test.py
@@ -1,26 +1,7 @@
-import os
-import matplotlib.pyplot as plt
 import cv2
 from mtcnn import MTCNN
-
-# img = cv2.imread("images/acne2.jpg")
-# grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#
-# plt.figure(figsize=(10,10))
-# plt.subplot(1,2,1)
-# plt.imshow(img);plt.axis('off');plt.title('Original Image')
-#
-# plt.subplot(1,2,2)
-# plt.imshow(grey, cmap='gray')
-# plt.axis('off');plt.title('Grayscale Image')
-#
-# plt.show()
-
-result_path = "result/extracted_facial"
-if not os.path.exists(result_path):
-    os.makedirs(result_path)
-
+import matplotlib.pyplot as plt
+# import os
 
 # โหลด MTCNN detector
 detector = MTCNN()
